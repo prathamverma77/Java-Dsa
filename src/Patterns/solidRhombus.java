c95154551c3ce7913ff6efe957e550b9870a425b
Pratham <prathamverma1980@gmail.com>
2026-04-11 20:33:39 +0530

    patterns practice

--- src/Patterns/solidRhombus.java
+++ src/Patterns/solidRhombus.java
@@ -3,22 +3,51 @@ package Patterns;
 public class solidRhombus {
 
     public static void main(String[] args) {
-        //printing a solid rhombus
-        int n = 5;
-        //first row
-        for (int row = 1; row<=n; row++){
-            //for each row gottta print spaces and then stars
+//        //printing a solid rhombus
+//        int n = 5;
+//        //first row
+//        for (int row = 1; row<=n; row++){
+//            //for each row gottta print spaces and then stars
+//
+//            //spaces
+//            for (int col = 1; col<=n-row; col++){
+//                System.out.print(" ");
+//            }
+//            //stars
+//            for (int col = 1; col<=n; col++){
+//                System.out.print("* ");
+//            }
+//            //moving to the next row
+//            System.out.println();
+//        }
 
-            //spaces
+
+
+        // reverse 90deg triangle
+//        int n = 5;
+//
+//        for (int row = 1; row<=n; row++){
+//            //first row of the stars
+//            for (int col= 1; col<=n-row+1; col++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+
+        //perfect triangle pattern
+        int n = 5;
+        for (int row = 1; row<=n; row++){
+
             for (int col = 1; col<=n-row; col++){
-                System.out.print(" ");
+                System.out.print("  ");
             }
-            //stars
-            for (int col = 1; col<=n; col++){
+            for (int col = 1; col<=2*row-1; col++){
                 System.out.print("* ");
             }
-            //moving to the next row
             System.out.println();
         }
+
+
     }
 }
