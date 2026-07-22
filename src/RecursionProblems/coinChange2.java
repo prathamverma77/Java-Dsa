4a5bd469f7830b45785e983fe54bea2e22db267a
Pratham <prathamverma1980@gmail.com>
2026-07-23 01:02:23 +0530

    update

--- src/RecursionProblems/coinChange2.java
+++ src/RecursionProblems/coinChange2.java
@@ -1,16 +1,16 @@
 package RecursionProblems;
 
-public class coinChange1 {
+public class coinChange2 {
 
-    static int coinChange1(int amount, int index, int[] coins) {
+    static int coinChange2(int amount, int index, int[] coins) {
         // Base cases (you got these right!)
         if (amount == 0) return 1;
         if (amount < 0) return 0;
         if (index == coins.length) return 0;
 
         // Recursive cases
-        int include_coin = coinChange1(amount - coins[index], index, coins);     // USE current coin
-        int exclude_coin = coinChange1(amount, index + 1, coins);               // SKIP current coin
+        int include_coin = coinChange2(amount - coins[index], index, coins);     // USE current coin
+        int exclude_coin = coinChange2(amount, index + 1, coins);               // SKIP current coin
 
         return include_coin + exclude_coin;
     }
@@ -18,7 +18,7 @@ public class coinChange1 {
     public static void main(String[] args) {
         int[] coins = {1, 2, 5};
         int amount = 5;
-        int ways = coinChange1(amount , 0 , coins);
+        int ways = coinChange2(amount , 0 , coins);
         System.out.println(ways);
     }
 
