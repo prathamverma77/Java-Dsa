78390eb7d2bf71a085a2f7c549b382d5186bafd0
Pratham <prathamverma1980@gmail.com>
2026-09-15 19:50:48 +0530

    linked lists

---
+++ src/LinkedLists/singlyLinkedLists.java
@@ -0,0 +1,32 @@
+package LinkedLists;
+
+public class singlyLinkedLists {
+
+    static class Node {
+        int data;
+        Node next;
+        //constructor
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    //constructor
+    public SinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+
+    }
+
+
+    //INSTERSTIONS all code functions
+
+    //insert at head function
+    
+
+}
---
+++ src/RecursionProblems/coinChange.java
@@ -0,0 +1,11 @@
+package RecursionProblems;
+
+public class coinChange {
+    static int coinChange(int amount , int index, int[] coins){
+        //base case
+        if(amount == 0){
+            return 0;
+        }
+        
+    }
+}
